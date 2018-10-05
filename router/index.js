877d61bdbbb7fb64f3172398e0d6a83c1775ddc7
vishal rana <vishal.rana@codezeros.com>
2018-10-05 19:33:43 +0530

    updated code

--- router/index.js
+++ router/index.js
@@ -9,6 +9,7 @@ var express = require('express');
 
 module.exports = function(app){
     console.log(process.cwd())
+    app.use('/',express.static(process.cwd()+'/public/dist/pluto'))
     app.use('/apiDocs',express.static(process.cwd()+'/api/dist'))
     app.use('/user',userRouter)
     app.use('/admin',adminRouter)
