6c076abcea5d6cd3028ada224f7d5e6547f94473
vishal rana <vishal.rana@codezeros.com>
2018-10-08 19:26:37 +0530

    updated responses

--- schema/user.js
+++ schema/user.js
@@ -25,8 +25,7 @@ var userSchema = new schema({
         state: String,
         pincode: Number
     },
-    // lat: { type: String },
-    // long: { type: String },
+    locationVisible:{type:Boolean,default:true},
     location:{
         type:{type:String},
         coordinates: [Number]
