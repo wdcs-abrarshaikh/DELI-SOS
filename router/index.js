496d08c12187a1eb5ab730589236f3b13e9dab22
vishal rana <vishal.rana@codezeros.com>
2018-10-23 11:44:33 +0530

    change express render setting

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 var userRouter  = require('../user/userRouter')
 var adminRouter = require('../admin/adminRouter')
-
+const path = require('path')
 var express = require('express');
 
 
@@ -8,7 +8,13 @@ var express = require('express');
 
 
 module.exports = function(app){
+    let distDir='../public/dist/pluto'
     app.use('/',express.static(process.cwd()+'/public/dist/pluto'))
+    app.use(express.static(path.join(__dirname, distDir)))
+    app.use(/^((?!(api)).)*/, (req, res) => {
+    res.sendFile(path.join(__dirname, distDir + '/index.html'));
+    });
+
     app.use('/apiDocs',express.static(process.cwd()+'/api/dist'))
     app.use('/user',userRouter)
     app.use('/admin',adminRouter)
