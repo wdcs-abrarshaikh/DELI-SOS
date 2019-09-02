c570b6b6ded12b2baca3e19c8da2c8ad404c2c1f
codezeros <abrarahmed.shaikh@dataeximit.com>
2019-09-02 14:30:43 +0530

    Delisos back-end : change url to https

--- public/src/app/app.service.ts
+++ public/src/app/app.service.ts
@@ -1,3 +1,3 @@
 
-export const URL:string="http://3.18.139.243:8080/";
-// export const URL:string="http://localhost:8080/";
+// export const URL:string="http://3.18.139.243:8080/";
+export const URL:string="https://delisos.com/";
